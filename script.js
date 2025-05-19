4622b10c8f1fa436fb3b57d67ae06c38221f2cbe
Am1r <158845341+itsRev1L@users.noreply.github.com>
2025-05-19 19:34:34 +0330

    responsive header

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuToggle = document.getElementById("menu-toggle");
+const sidebar = document.getElementById("sidebar");
+const closeBtn = document.getElementById("close-btn");
+
+menuToggle.addEventListener("click", () => {
+  sidebar.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+});
